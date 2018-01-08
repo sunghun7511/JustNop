d26f470096642dcb3a9db35b6ef2e29a3b9e7154
SHGroup <sunghun7511@naver.com>
2018-01-09 00:31:46 +0900

    Haha :D Just NOP!

---
+++ src/com/SHGroup/JUSTNOP/NOPMain.java
@@ -0,0 +1,13 @@
+package com.SHGroup.JUSTNOP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NOPMain extends JavaPlugin{
+	public void onEnable() {
+		System.out.println("진짜 아무것도 아닌 Plug-in ㅎㅎ");
+	}
+	
+	public void onDisable() {
+		System.out.println("진짜 아무것도 아닌 Plug-out ㅎㅎ");
+	}
+}
